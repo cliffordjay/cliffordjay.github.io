9f118a57ef87092cd2799351937767eb0274c5c4
cliffordjay <cliffordjay27@gmail.com>
2024-05-25 10:19:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const btnLike2 = document.getElementById("btnLike2")
 const count2 = document.getElementById("count2")
 
 function countLike2() {
-  let likeCounts = parseInt(count1.value) + 1
+  let likeCounts = parseInt(count2
+                            .value) + 1
   count2.textContent = likeCounts.toString()
 }
 
